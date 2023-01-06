1d4b4efbfdea50d338fe8d01ab7e50f7e60faec0
agentlans <at92001@gmail.com>
2023-01-06 17:28:00 -0500

    Improve explanation

--- Example.cpp
+++ Example.cpp
@@ -2,19 +2,22 @@
 #include "AhoCorasick.hpp"
 
 void callback(int word_index, unsigned long start, unsigned long end) {
-    std::cout << "Found word " << word_index << " at " << start << " to " << end << std::endl;
+    std::cout << "Found word " << word_index << 
+    	" at index [" << start << ", " << end << ")" << std::endl;
 }
 
 int main() {
     Trie<char> ac;
-    ac.add("He");
-    ac.add("Hello");
-    ac.add("HelloWorld");
-    ac.add("loW");
+    ac.add("He"); // word 0
+    ac.add("Hello"); // word 1
+    ac.add("HelloWorld"); // word 2
+    ac.add("loW"); // word 3
     ac.finish();
 
     AhoCorasick<char> bar1(ac, callback);
     char s[] = "11234HelloHelloWorld1234";
+    // Feed the string one character at a time into the automaton
+    // The callback will be called automatically.
     for (const auto& ch : s) {
         bar1.next(ch);
     }
